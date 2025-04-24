47b0e6e3f018010464eafe34ea913a5bbebe61a2
Sergey_Begiyan <124567856+serbegiyan@users.noreply.github.com>
2025-04-24 22:14:21 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,6 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
